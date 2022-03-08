d34a630b3bd5759896ae8d18ae8d442faa41ad55
Freddy Silber <freddy.silber@cloudcoach.com>
2022-03-07 21:11:54 -0700

    clean up

--- src/hover.ts
+++ src/hover.ts
@@ -1,39 +1,33 @@
-/**
- * Notes
- * 
- * Does this need to be a const?
- * Can this be an exported function or a class???
- * Can this be a default export?
- */
-import * as vscode from 'vscode';
+import {
+	languages,
+	ExtensionContext,
+	MarkdownString,
+	Hover,
+	Range
+} from 'vscode';
 
+// Keep this as a class for now, so we can operate within it, but I think it could be a function at the end of the day
 export class DISpI {
-	constructor(context: vscode.ExtensionContext) {
+	constructor(context: ExtensionContext) {
 		console.log('CONTEXT', context);
 
-		vscode.languages.registerHoverProvider('typescript', {
+		languages.registerHoverProvider('typescript', {
 			provideHover(document, position, token) {
 				console.log({ document, position, token });
-				const markdown = new vscode.MarkdownString(`
-|    <span style="color:#ff0;background-color:#000;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Table&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>|    Header     |
-|    :----:    |    :----:     |
-|first cell    |second cell  |
-|third cell    |<span style="color:#f00;background-color:#fff;">&nbsp;&nbsp;fourth cell&nbsp;&nbsp;</span>  |
-        \n\n\n`);
-				const codeblock = 'const a = 12';
-				markdown.appendCodeblock(codeblock, 'typescript');
-				return new vscode.Hover(markdown, new vscode.Range(position, position));
-				// return {
-				// 	contents: [
-				// 		'Hover Content',
-				// 		'yo',
-				// 		'```public readonly _dep: MyDep```'
-				// 	]
-				// };
+				console.log(document.getText());
+				// console.log(document.getWordRangeAtPosition());
+				const markdown = new MarkdownString(
+					// We can use markdown styling to style this stuff better...
+					`<span style="background-color: #eb4034;">Mr Sir</span>`
+				);
+
+				markdown.appendCodeblock(
+					`const a = 12 // here is a thing`,
+					'typescript'
+				);
+
+				return new Hover(markdown, new Range(position, position));
 			}
 		});
-		// const input = vscode.window.showInputBox().then((input) => {
-		// 	console.log(input);
-		// });
 	}
 }
